40b1a84f6c4b9c31b7be71c4ba7fc0821be8c244
Joseph Pagliuca <51335923+Otterboi@users.noreply.github.com>
2026-05-15 19:29:45 -0400

    created ProjectList component

---
+++ app/components/ProjectList.tsx
@@ -0,0 +1,11 @@
+export default function ProjectList(){
+    return(
+        <div className="grid grid-cols-4 gap-4">
+            <div>01</div>
+            <div>02</div>
+            <div>03</div>
+            <div>04</div>
+            <div>05</div>
+        </div>
+    )
+}
