994d3046e510ca89e2951c0708bc7c887930326e
Joseph Pagliuca <51335923+Otterboi@users.noreply.github.com>
2026-05-16 11:11:12 -0400

    added project place holders with css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import ProjectList from "./components/ProjectList";
+import projects from "./projects/projects.json"
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
-        <ProjectList></ProjectList>
+        <div className="grid grid-cols-3 gap-5">
+          {projects.map((project, index) => (
+            <ProjectList 
+            key={index} 
+            name={project.project_name}
+            description={project.project_description}
+            link={project.project_link}
+            />
+          ))}
+        </div>
       </main>
     </div>
   );
