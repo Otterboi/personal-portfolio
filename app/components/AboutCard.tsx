b7ccdba0491c976bca5f5b626a6fe3bd8aa0ffcd
Joseph Pagliuca <51335923+Otterboi@users.noreply.github.com>
2026-05-23 17:47:59 -0400

    moved email and linkedin buttons

--- app/components/AboutCard.tsx
+++ app/components/AboutCard.tsx
@@ -37,11 +37,11 @@ export default function AboutCard() {
           <a href="#" className="rounded-md bg-slate-800 py-3 px-6 text-sm text-white shadow-md hover:bg-slate-700 transition text-center">
             GitHub
           </a>
-          <a href="mailto:your.email@example.com" className="rounded-md bg-slate-800 py-3 px-6 text-sm text-white shadow-md hover:bg-slate-700 transition text-center">
-            Email
-          </a>
           <a href="#" className="rounded-md bg-slate-800 py-3 px-6 text-sm text-white shadow-md hover:bg-slate-700 transition text-center">
             LinkedIn
+          </a>
+          <a href="mailto:your.email@example.com" className="rounded-md bg-slate-800 py-3 px-6 text-sm text-white shadow-md hover:bg-slate-700 transition text-center">
+            Email
           </a>
           </div>
         </div>
