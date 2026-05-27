214e586d677952aca0ae390b24368e76215b221c
Joseph Pagliuca <51335923+Otterboi@users.noreply.github.com>
2026-05-27 17:21:45 -0400

    updated header text and added icons Changed the header to now contain icons, left justified when on desktop and centered when on mobile while bolding the text,

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Home, Settings, Folder } from "lucide-react"
 
 export default function Header() {
   const [showHeader, setShowHeader] = useState(true);
@@ -28,14 +29,17 @@ export default function Header() {
 
   return (
     <header className={`fixed top-0 left-0 w-full z-50 bg-white border-b border-slate-200 transition-transform duration-300 ${showHeader ? "translate-y-0" : "-translate-y-full"}`}>
-      <nav className="w-full px-4 sm:px-6 lg:px-12 py-4 flex justify-center gap-8">
-        <a href="#home" className="text-slate-600 hover:text-slate-900 font-medium transition">
+      <nav className="max-w-7xl mx-auto py-4 flex justify-center sm:justify-start gap-12">
+        <a href="#home" className="text-slate-600 hover:text-slate-900 font-semibold text-lg transition flex item-center gap-2">
+          <Home />
           Home
         </a>
-        <a href="#skills" className="text-slate-600 hover:text-slate-900 font-medium transition">
+        <a href="#skills" className="text-slate-600 hover:text-slate-900 font-semibold text-lg transition flex item-center gap-2">
+          <Settings />
           Skills
         </a>
-        <a href="#projects" className="text-slate-600 hover:text-slate-900 font-medium transition">
+        <a href="#projects" className="text-slate-600 hover:text-slate-900 font-semibold text-lg transition flex item-center gap-2">
+          <Folder />
           Projects
         </a>
       </nav>
